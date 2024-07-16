2cd10b4e9d606dc85354dd61804754f564273010
EXELVI <exelvi@outlook.com>
2024-07-17 00:45:28 +0200

    chore: Added comments to the code and created the readme file

--- ArcadeTime_OLED.ino
+++ ArcadeTime_OLED.ino
@@ -9,16 +9,19 @@
 #include <Wire.h>
 #include <NTPClient.h>
 
-ArduinoLEDMatrix matrix;
+auto timeZoneOffsetHours = 2; // Time zone offset in hours from UTC 
 
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
 
-#include "icons.h"
+ArduinoLEDMatrix matrix; // Create an instance of the matrix
+
+#define OLED_RESET 4 
+Adafruit_SSD1306 display(OLED_RESET); // Create an instance of the display
+
+#include "icons.h" // Include the icons
 // Array of all bitmaps for convenience. (Total bytes used to store images in PROGMEM = 480 + 336)
 const int epd_bitmap_allArray_LEN = 5;
 const unsigned char *epd_bitmap_allArray[8] = { // 32x23px
-    epd_bitmap_check_lg,
+    epd_bitmap_check_lg, 
     epd_bitmap_exclamation_octagon_fill,
     epd_bitmap_pause_fill,
     epd_bitmap_stopwatch,
@@ -27,9 +30,9 @@ const unsigned char *epd_bitmap_allArray[8] = { // 32x23px
     epd_bitmap_hourglass_split,
     epd_bitmap_hourglass_top};
 
-const unsigned char *getIcon(const char *icon)
+const unsigned char *getIcon(const char *icon) // Function to get the icon
 {
-  if (strcmp(icon, "error") == 0)
+  if (strcmp(icon, "error") == 0) // strcmp means string compare, 0 when equal 
   {
     return epd_bitmap_allArray[1];
   }
@@ -59,92 +62,89 @@ const unsigned char *getIcon(const char *icon)
   }
   else if (strcmp(icon, "hourglass_top") == 0)
   {
-    return epd_bitmap_allArray[7];
+    return epd_bitmap_allArray[7]; 
   }
   else
   {
-    return epd_bitmap_allArray[4];
+    return epd_bitmap_allArray[4]; // Default to x icon
   }
 }
 
-#include "WiFiS3.h"
-#include <WiFiUdp.h>
-#include <Arduino_JSON.h>
+#include "WiFiS3.h" // Include the WiFiS3 librar y
+#include <WiFiUdp.h> // Include the WiFiUdp library for NTP
+#include <Arduino_JSON.h> // Include the Arduino_JSON library
 
-const char *host = "hackhour.hackclub.com";
-#include "arduino_secrets.h"
+const char *host = "hackhour.hackclub.com"; // Host 
+#include "arduino_secrets.h" // Include the secrets file
 
-int wifiStatus = WL_IDLE_STATUS;
-WiFiUDP Udp;
-NTPClient timeClient(Udp);
+int wifiStatus = WL_IDLE_STATUS; // WiFi status
+WiFiUDP Udp; // Create an instance of the WiFiUDP class
+NTPClient timeClient(Udp); // Create an instance of the NTPClient class
 
-unsigned long lastConnectionTime = 0;
-const unsigned long postingInterval = 10000;
-JSONVar myObject;
-JSONVar jsonResult;
+unsigned long lastConnectionTime = 0; // Last connection time
+const unsigned long postingInterval = 10000; // Posting interval
+JSONVar jsonResult; // Create an instance of the JSONVar class
 
-auto timeZoneOffsetHours = 2;
+char ssid[] = SECRET_SSID; // SSID (edit in arduino_secrets.h)
+char pass[] = SECRET_PASS; // Password (edit in arduino_secrets.h)
+char *apiKey = SECRET_API_KEY; // API key (edit in arduino_secrets.h)
+char *slackid = SECRET_SLACK_ID; // Slack ID (edit in arduino_secrets.h)
+int keyIndex = 0; // your network key index number (needed only for WEP) 
 
-char ssid[] = SECRET_SSID;
-char pass[] = SECRET_PASS;
-char *apiKey = SECRET_API_KEY;
-char *slackid = SECRET_SLACK_ID;
-int keyIndex = 0; // your network key index number (needed only for WEP)
+int status = WL_IDLE_STATUS; // WiFi status
 
-int status = WL_IDLE_STATUS;
+WiFiClient client; // Create an instance of the WiFiClient class
 
-WiFiClient client;
-
-long parseISO8601(const char *dateTime)
+long parseISO8601(const char *dateTime) // Function to parse ISO8601 date time (since c does not have a built-in function I created one)
 {
-  int year, month, day, hour, minute, second;
-  sscanf(dateTime, "%4d-%2d-%2dT%2d:%2d:%2dZ", &year, &month, &day, &hour, &minute, &second);
+  int year, month, day, hour, minute, second; // Variables to store the year, month, day, hour, minute, and second
+  sscanf(dateTime, "%4d-%2d-%2dT%2d:%2d:%2dZ", &year, &month, &day, &hour, &minute, &second); // Parse the date time
 
-  struct tm timeinfo;
-  timeinfo.tm_year = year - 1900;
-  timeinfo.tm_mon = month - 1;
-  timeinfo.tm_mday = day;
-  timeinfo.tm_hour = hour;
-  timeinfo.tm_min = minute;
-  timeinfo.tm_sec = second;
+  struct tm timeinfo; // Create an instance of the tm struct
+  timeinfo.tm_year = year - 1900; // Set the year (since tm_year is years since 1900) 
+  timeinfo.tm_mon = month - 1; // Set the month (since tm_mon is months since January)
+  timeinfo.tm_mday = day; // Set the day
+  timeinfo.tm_hour = hour; // Set the hour 
+  timeinfo.tm_min = minute; // Set the minute
+  timeinfo.tm_sec = second; // Set the second
 
-  time_t t = mktime(&timeinfo);
-  return (long)t;
+  time_t t = mktime(&timeinfo); // Convert the tm struct to a time_t value
+  return (long)t; // Return the time_t value as a long 
 }
 
-void repeatString(const char *str, int times, char *result)
+void repeatString(const char *str, int times, char *result) // Function to repeat a string (inspired from JS repeat function) 
 {
-  result[0] = '\0';
+  result[0] = '\0'; // Set the result to an empty but not null string
 
-  for (int i = 0; i < times; ++i)
+  for (int i = 0; i < times; ++i) // Loop through the times
   {
-    strcat(result, str);
+    strcat(result, str); // Concatenate the string to the result
   }
-}
+} 
 
 void setup()
 {
 
-  RTC.begin();
-  Serial.begin(9600);
-  while (!Serial)
+  RTC.begin(); // Begin the RTC
+  Serial.begin(9600); // Begin the serial monitor
+  while (!Serial) // Wait for the serial monitor to open
   {
-    ;
+    ; 
   }
-
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
+  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Begin the display 
+  display.clearDisplay(); 
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
   display.print("Arduino Arcade Time");
-  display.drawBitmap(0, 7, getIcon("stopwatch"), 32, 23, WHITE);
-  display.display();
+  display.drawBitmap(0, 7, getIcon("stopwatch"), 32, 23, WHITE); // Draw the stopwatch icon
+  display.display(); 
 
-  delay(5000);
-  if (WiFi.status() == WL_NO_MODULE)
+  delay(5000); 
+  if (WiFi.status() == WL_NO_MODULE) // Check if the WiFi module is not present
   {
-    Serial.println("Communication with WiFi module failed!");
+    Serial.println("Communication with WiFi module failed!"); 
     display.clearDisplay();
     display.setCursor(0, 0);
     display.print("Communication with WiFi module failed!");
@@ -153,34 +153,34 @@ void setup()
       ;
   }
 
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+  String fv = WiFi.firmwareVersion(); // Get the firmware version
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION)  // Check if the firmware version is less than the latest version
   {
-    Serial.println("Please upgrade the firmware");
+    Serial.println("Please upgrade the firmware"); 
     display.clearDisplay();
     display.setCursor(0, 0);
     display.print("Please upgrade the firmware");
     display.display();
   }
 
-  int frame = 0;
-  const char *dot = ".";
-  while (status != WL_CONNECTED)
+  int frame = 0;  // Frame counter
+  const char *dot = "."; // Dot string
+  while (status != WL_CONNECTED) // Check if the WiFi is not connected
   {
 
-    char result[100];
-    repeatString(dot, frame + 1, result);
+    char result[100]; 
+    repeatString(dot, frame + 1, result); // Repeat the dot string
 
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
     display.clearDisplay();
     display.setCursor(35, 10);
-    display.print("Connecting" + String(result));
+    display.print("Connecting" + String(result)); 
     display.setCursor(35, 20);
     display.print(ssid);
-    if (frame == 0)
+    if (frame == 0) 
     {
-      display.drawBitmap(0, 10, getIcon("hourglass_top"), 32, 23, WHITE);
+      display.drawBitmap(0, 10, getIcon("hourglass_top"), 32, 23, WHITE); 
     }
     else if (frame == 1)
     {
@@ -190,30 +190,30 @@ void setup()
     {
       display.drawBitmap(0, 10, getIcon("hourglass_bottom"), 32, 23, WHITE);
     }
-    display.display();
-    frame++;
-    if (frame > 2)
+    display.display();   
+    frame++; 
+    if (frame > 2) // If frame is greater than 2 reset it to 0 
     {
-      frame = 0;
+      frame = 0; 
     }
-    status = WiFi.begin(ssid, pass);
+    status = WiFi.begin(ssid, pass); // Begin the WiFi connection 
   }
 
-  printWifiStatus();
+  printWifiStatus(); // Print the WiFi status
   delay(5000);
   Serial.println("\nStarting connection to server...");
-  timeClient.begin();
-  timeClient.update();
+  timeClient.begin(); // Begin the time client
+  timeClient.update(); // Update the time client
 
-  auto unixTime = timeClient.getEpochTime();
-  Serial.print("Unix time = ");
+  auto unixTime = timeClient.getEpochTime(); // Get the Unix time
+  Serial.print("Unix time = "); 
   Serial.println(unixTime);
-  RTCTime timeToSet = RTCTime(unixTime);
-  RTC.setTime(timeToSet);
+  RTCTime timeToSet = RTCTime(unixTime); // Create an instance of the RTCTime class
+  RTC.setTime(timeToSet); // Set the RTC time
 
-  RTCTime currentTime;
-  RTC.getTime(currentTime);
-  Serial.println("The RTC was just set to: " + String(currentTime));
+  RTCTime currentTime; // Create an instance of the RTCTime class
+  RTC.getTime(currentTime); 
+  Serial.println("The RTC was just set to: " + String(currentTime)); 
 
   http_request();
 }
@@ -222,16 +222,18 @@ void setup()
 void loop()
 {
   // Bars
-  RTCTime currentTime;
+RTCTime currentTime; 
 
-  RTC.getTime(currentTime);
-  display.clearDisplay();
-  printWifiBar();
-  display.setCursor(0, 0);
+RTC.getTime(currentTime); 
+display.clearDisplay(); 
+printWifiBar(); 
+display.setCursor(0, 0);
 
-  String hours = String(currentTime.getHour() + timeZoneOffsetHours);
-  String minutes = String(currentTime.getMinutes());
-  String seconds = String(currentTime.getSeconds());
+int adjustedHour = (currentTime.getHour() + timeZoneOffsetHours) % 24;
+String hours = String(adjustedHour); 
+String minutes = String(currentTime.getMinutes());
+String seconds = String(currentTime.getSeconds());
+
   display.print(hours.length() == 1 ? "0" + hours : hours);
   display.print(":");
   display.print(minutes.length() == 1 ? "0" + minutes : minutes);
@@ -239,24 +241,24 @@ void loop()
   display.print(seconds.length() == 1 ? "0" + seconds : seconds);
   // Body
 
-  if (jsonResult.hasOwnProperty("ok"))
+  if (jsonResult.hasOwnProperty("ok")) // Check if the JSON result has the property "ok" 
   {
-    if ((bool)jsonResult["ok"] == true)
+    if ((bool)jsonResult["ok"] == true) // Check if the value of the "ok" property is true
     {
-      JSONVar data = jsonResult["data"];
+      JSONVar data = jsonResult["data"]; // Get the data property
 
-      if (data["completed"])
+      if (data["completed"]) 
       {
         display.setCursor(36, 10);
         display.print("No session");
-        display.drawBitmap(0, 10, getIcon("x"), 32, 23, WHITE);
+        display.drawBitmap(0, 10, getIcon("x"), 32, 23, WHITE); 
       }
-      else if ((bool)data["paused"])
+      else if ((bool)data["paused"]) 
       {
-        String goal = data["goal"];
-        if (goal.length() > 10)
+        String goal = data["goal"]; 
+        if (goal.length() > 10) // If goal exceeds 10 characters truncate it to prevent overflow
         {
-          goal = goal.substring(0, 10) + "...";
+          goal = goal.substring(0, 10) + "..."; // cut the string to 10 characters and add "..."
         }
         display.setCursor(36, 10);
         display.setTextSize(2);
@@ -264,15 +266,15 @@ void loop()
         display.setTextSize(1);
         display.setCursor(36, 25);
         display.print(goal);
-        display.drawBitmap(0, 10, getIcon("pause"), 32, 23, WHITE);
+        display.drawBitmap(0, 10, getIcon("pause"), 32, 23, WHITE); 
       }
       else
       {
-        long remaining = parseISO8601(data["endTime"]) - currentTime.getUnixTime();
-        int minutesR = remaining / 60;
-        int secondsR = remaining % 60;
+        long remaining = parseISO8601(data["endTime"]) - currentTime.getUnixTime();  // Calculate the remaining time
+        int minutesR = remaining / 60; 
+        int secondsR = remaining % 60;  // the % operator returns the remainder of the division
 
-        if (secondsR < 0)
+        if (secondsR < 0) 
         {
           display.setCursor(36, 10);
           display.print("Session ended");
@@ -283,13 +285,13 @@ void loop()
           String goal = data["goal"];
           if (goal.length() > 10)
           {
-            goal = goal.substring(0, 10) + "...";
+            goal = goal.substring(0, 10) + "..."; 
           }
 
-          char remainingMinStr[3];
+          char remainingMinStr[3]; 
           char remainingSecStr[3];
-          sprintf(remainingMinStr, "%02d", minutesR);
-          sprintf(remainingSecStr, "%02d", secondsR);
+          sprintf(remainingMinStr, "%02d", minutesR); // adds 0 padding to the minutes (instead of 1 it will be 01, 2 will be 02, etc.)
+          sprintf(remainingSecStr, "%02d", secondsR);  // adds 0 padding to the seconds (same)
 
           display.setCursor(36, 10);
           display.setTextSize(2);
@@ -300,7 +302,7 @@ void loop()
           display.setCursor(36, 25);
           display.print(goal);
         }
-        display.drawBitmap(0, 10, getIcon("stopwatch"), 32, 23, WHITE);
+        display.drawBitmap(0, 10, getIcon("stopwatch"), 32, 23, WHITE); 
       }
     }
     else
@@ -314,63 +316,63 @@ void loop()
   }
 
   display.display();
-
-  read_response();
-  if (millis() - lastConnectionTime > postingInterval)
+ 
+  read_response(); 
+  if (millis() - lastConnectionTime > postingInterval) // Check if the time since the last connection is greater than the posting interval, I don't like using delay() because it blocks the code
   {
-    http_request();
+    http_request(); 
   }
 }
-
-void read_response()
+ 
+void read_response() // Function to read the response
 {
-  if (client.available())
+  if (client.available()) // Check if the client is available (has data to read)
   {
-    Serial.println("reading response");
-    String line = client.readStringUntil('\r');
+    Serial.println("reading response"); 
+    String line = client.readStringUntil('\r'); // Read the response until a carriage return 
 
     Serial.print(line);
-    if (line == "\n")
+    if (line == "\n") // Check if the line is a new line
     {
       Serial.println("headers received");
-      String payload = client.readString();
-      myObject = JSON.parse(payload);
-      jsonResult = myObject;
-      Serial.println(jsonResult);
+      String payload = client.readString(); // Read the payload
+      jsonResult = JSON.parse(payload); // Parse the payload
+      Serial.println(jsonResult);  
     }
   }
 }
 
-void http_request()
+void http_request() // Function to make the HTTP request
 {
 
-  client.stop();
+  client.stop(); // Stop the client (if it is already connected)
 
-  Serial.println("\nStarting connection to server...");
-  if (client.connect(host, 80))
-  {
+  Serial.println("\nStarting connection to server..."); 
+  if (client.connect(host, 80)) // Connect to the host
+  { 
     Serial.println("connected to server");
-    String request = "/api/session/" + String(slackid);
-    client.println("GET " + request + " HTTP/1.1");
-    client.println("Host: " + String(host));
-    client.println("Authorization: Bearer " + String(apiKey));
-    client.println("Connection: close");
-    client.println();
-    lastConnectionTime = millis();
+    String request = "/api/session/" + String(slackid); // request path
+    client.println("GET " + request + " HTTP/1.1");  // Send the GET request
+    client.println("Host: " + String(host)); 
+    client.println("Authorization: Bearer " + String(apiKey)); 
+    client.println("Connection: close"); 
+    client.println(); 
+    lastConnectionTime = millis(); // Set the last connection time to the current time 
   }
-  else
-  {
+  else 
+  { // If the connection failed
     Serial.println("connection failed");
   }
 }
 
-void printWifiBar()
+void printWifiBar() // Function to print the WiFi bar
 {
-  long rssi = WiFi.RSSI();
+  long rssi = WiFi.RSSI(); // Get the RSSI (Received Signal Strength Indicator)
 
-  if (rssi > -55)
+  // It is a simple function that draws a bar based on the signal strength
+  if (rssi > -55) 
   {
-    display.fillRect(102, 7, 4, 1, WHITE);
+    display.fillRect(102, 7, 4, 1, WHITE); 
     display.fillRect(107, 6, 4, 2, WHITE);
     display.fillRect(112, 4, 4, 4, WHITE);
     display.fillRect(117, 2, 4, 6, WHITE);
@@ -418,12 +420,11 @@ void printWifiBar()
   }
 }
 
-void printWifiStatus()
+void printWifiStatus() // Function to print the WiFi status
 {
-
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
-  display.clearDisplay();
+  display.clearDisplay(); 
   display.setCursor(0, 0);
   display.print(String(WiFi.SSID()));
 
---
+++ README.md
@@ -0,0 +1,33 @@
+# Arduino Arcade Time
+
+An arduino project for the [Arcade](https://hackclub.com/arcade) timer 
+
+## Photos
+
+
+![Screen 1](screen1.jpg)
+![Screen 0](screen0.jpg)
+
+## Components
+
+- Arduino Uno R4 WiFi
+- 0.96" OLED Display I2C
+
+## Pins
+
+| Pin | Function |
+| --- | --- |
+| 5V | VCC |
+| GND | GND |
+| SDA | SDA |
+| SCL | SCL |
+
+## Usage
+
+1. Connect the OLED Display to the Arduino Uno
+2. Rename and edit the `arduino_secrets.h.example` file to `arduino_secrets.h` and add your WiFi credentials and the API id and key
+3. Upload the code to the Arduino Uno
+
+## Contributing
+
+Feel free to contribute to this project and make it better
